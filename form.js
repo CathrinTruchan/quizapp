535591b5b089ff801c57fd74f3f480e7d0338b3d
Cathrin Truchan <cathrin.truchan@gmail.com>
2022-09-08 11:46:46 +0200

    add new file form.js

--- form.js
+++ form.js
@@ -1,36 +1,3 @@
-const myCard = document.querySelector('[data-js="card"]');
-const myButton = document.querySelector('[data-js="button"]');
-const myQuestion = document.querySelector('[data-js="question"]');
-const myAnswer = document.querySelector('[data-js="answer"]');
-const myBookmark = document.querySelector('[data-js="bookmark"]');
-
-// myButton.addEventListener("click", () => {
-// myButton.classList.toggle("card__button-hide");
-// myQuestion.classList.toggle("card__text--hidden");
-// myAnswer.classList.toggle("card__text--hidden");
-// myQuestion.classList.toggle("card__text--hidden");
-// });
-
-// Funktionalität Card
-
-myButton.addEventListener("click", () => {
-  if (myButton.textContent === "Show Answer") {
-    myButton.classList.add("card__button-hide");
-    myButton.textContent = "Hide Answer";
-    myQuestion.classList.add("card__text--hidden");
-    myAnswer.classList.remove("card__text--hidden");
-  } else {
-    myButton.classList.remove("card__button-hide");
-    myButton.textContent = "Show Answer";
-    myQuestion.classList.remove("card__text--hidden");
-    myAnswer.classList.add("card__text--hidden");
-  }
-});
-
-myBookmark.addEventListener("click", () => {
-  myBookmark.classList.toggle("card__bookmark--active");
-});
-
 // Funktionalität Form
 
 const form = document.querySelector('[data-js="form"]');
